509165becc84ed3a2a7abe24a3901afdf65b7be8
Vitor Soares <pereiravitor1218@gmail.com>
2022-10-11 15:39:10 -0300

    refac: home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,74 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import * as Dialog from '@radix-ui/react-dialog';
+import { GetServerSideProps } from 'next';
+import Image from 'next/image';
+import prisma from '../lib/prisma';
 
-const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      Hello word!
-    </div>
-  )
+import { GameBanner } from '../components/GameBanner';
+
+import { CreateAdBanner } from '../components/CreateAdBanner';
+import { CreateAdModal } from '../components/CreateAdModal';
+import logoImg from '../public/logo-nlw-esports.svg';
+
+export interface Game {
+  games: {
+    bannerUrl: string;
+    id: string;
+    title: string;
+    _count: {
+      ads: number;
+    };
+  }[];
 }
 
-export default Home
+const Home = ({ games }: Game) => {
+  return (
+    <div className='w-full min-h-screen bg-[#121214] bg-galaxy bg-cover bg-no-repeat mx-auto flex flex-col items-center px-20 py-5'>
+      <Image
+        src={logoImg}
+        alt=''
+      />
+
+      <h1 className='text-6xl text-white font-black mt-12'>
+        Seu{' '}
+        <span className='bg-nlw-gradient bg-clip-text text-transparent'>
+          duo
+        </span>{' '}
+        está aqui.
+      </h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-14'>
+        {games.map((game) => (
+          <GameBanner
+            key={game.id}
+            bannerUrl={game.bannerUrl}
+            title={game.title}
+            adsCount={game._count.ads}
+          />
+        ))}
+      </div>
+      <Dialog.Root>
+        <CreateAdBanner />
+
+        <CreateAdModal games={games} />
+      </Dialog.Root>
+    </div>
+  );
+};
+
+export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+
+  return {
+    props: { games },
+  };
+};
