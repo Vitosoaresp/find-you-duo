eb591ff182707b637b8f867d2442ea075a867db1
Vitor Soares <pereiravitor1218@gmail.com>
2022-10-11 21:07:48 -0300

    feat:  slider in gamesBanner

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import { GetServerSideProps } from 'next';
 import Image from 'next/image';
 import prisma from '../lib/prisma';
 
+import 'keen-slider/keen-slider.min.css';
+import { useKeenSlider } from 'keen-slider/react';
+
 import { GameBanner } from '../components/GameBanner';
 
 import { CreateAdBanner } from '../components/CreateAdBanner';
@@ -21,14 +24,37 @@ export interface Game {
 }
 
 const Home = ({ games }: Game) => {
+  const [ref] = useKeenSlider<HTMLDivElement>({
+    loop: true,
+    mode: 'free-snap',
+    slides: {
+      perView: 6,
+      spacing: 24,
+    },
+    breakpoints: {
+      '(max-width: 1024px)': {
+        slides: {
+          perView: 4,
+          spacing: 24,
+        },
+      },
+      '(max-width: 640px)': {
+        slides: {
+          perView: 2,
+          spacing: 24,
+        },
+      },
+    },
+  });
+
   return (
-    <div className='w-full min-h-screen bg-[#121214] bg-galaxy bg-cover bg-no-repeat mx-auto flex flex-col items-center px-20 py-5'>
+    <div className='w-full min-h-screen bg-[#121214] bg-galaxy bg-cover bg-no-repeat mx-auto flex flex-col items-center md:px-20 px-4 py-5'>
       <Image
         src={logoImg}
         alt=''
       />
 
-      <h1 className='text-6xl text-white font-black mt-12'>
+      <h1 className='md:text-6xl text-3xl text-white font-black mt-12'>
         Seu{' '}
         <span className='bg-nlw-gradient bg-clip-text text-transparent'>
           duo
@@ -36,7 +62,10 @@ const Home = ({ games }: Game) => {
         está aqui.
       </h1>
 
-      <div className='grid grid-cols-6 gap-6 mt-14'>
+      <div
+        className='keen-slider flex mt-14'
+        ref={ref}
+      >
         {games.map((game) => (
           <GameBanner
             key={game.id}
